0bd538bf418a27ed7082852a9a3136a50d189e16
Ryan Boris <19439753+secantsquared@users.noreply.github.com>
2020-06-26 02:17:21 -0400

    :tada: Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://limitless-wildwood-37360.herokuapp.com/api/grid/data")
+      .then(res => res.json())
+      .then(r => setData(r))
+      .catch(err => {
+        if (err) throw err;
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   setInterval(updateGrid, 200);
+  // }, []);
+
+  function updateGrid() {
+    fetch("https://limitless-wildwood-37360.herokuapp.com/update")
+      .then(res => res.json())
+      .then(r => setData(r))
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
+  return (
+    <table className="Grid">
+      <tbody>
+        {Object.entries(data).map(([k, v]) => (
+          <tr key={k}>
+            {v.map((item, idx) => (
+              <td className={item === 0 ? "white" : "black"} key={idx}>
+                {item}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
