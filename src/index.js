f44caea85925d36de423b9db9f53ff0f022cc60b
Ryan Boris <19439753+secantsquared@users.noreply.github.com>
2020-06-26 02:46:10 -0400

    update

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import "./index.css";
 
 const App = () => {
   const [data, setData] = useState({});
-
+  const [initial, setInitial] = useState({});
   useEffect(() => {
     fetch("https://limitless-wildwood-37360.herokuapp.com/api/grid/data")
       .then(res => res.json())
-      .then(r => setData(r))
+      .then(r => {
+        setData(r);
+        setInitial(r);
+      })
       .catch(err => {
         if (err) throw err;
       });
@@ -28,20 +31,32 @@ const App = () => {
       });
   }
 
+  function reset() {
+    fetch("https://limitless-wildwood-37360.herokuapp.com/reset")
+      .then(res => res.json())
+      .then(r => r)
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
   return (
-    <table className="Grid">
-      <tbody>
-        {Object.entries(data).map(([k, v]) => (
-          <tr key={k}>
-            {v.map((item, idx) => (
-              <td className={item === 0 ? "white" : "black"} key={idx}>
-                {item}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table className="Grid">
+        <tbody>
+          {Object.entries(data).map(([k, v]) => (
+            <tr key={k}>
+              {v.map((item, idx) => (
+                <td className={item === 0 ? "white" : "black"} key={idx}>
+                  {item}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={reset}>RESET</button>
+    </>
   );
 };
 
